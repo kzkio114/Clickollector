a6db64118282ba1e995269046e326795e2ba177e
伊藤 一希 <kzkio114@gmail.com>
2024-03-09 17:53:45 +0900

    修正

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="App">
+          <h1 className="text-3xl font-bold underline">Hello World</h1>
+          <button className="btn btn-primary">Button</button>
+        </div>
+      );
+  
 }
 
 export default App;
