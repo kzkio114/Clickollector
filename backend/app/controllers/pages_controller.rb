aae279e3388ab9fc1996c4a7b699ebedb10b1d64
伊藤 一希 <kzkio114@gmail.com>
2024-03-10 01:53:14 +0900

    ボタン押したらなんか出た

--- backend/app/controllers/pages_controller.rb
+++ backend/app/controllers/pages_controller.rb
@@ -1,8 +1,5 @@
 class PagesController < ApplicationController
-  include GoogleApiConcern
 
-   before_action :initialize_google_api, only: [:index]
- 
     def index
     end
 
--- frontend/src/ImageGallery.jsx
+++ frontend/src/ImageGallery.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 function ImageGallery() {
   const [images, setImages] = useState([]);
   const [selectedImage, setSelectedImage] = useState('');
-  const [showImage, setShowImage] = useState(false);
+  const [showImage, setShowImage] = useState(false); // 最初はfalseにしておく
 
   useEffect(() => {
     fetch('http://localhost:3000/api/v1/images')
@@ -12,34 +12,28 @@ function ImageGallery() {
       .catch(error => console.error('Error fetching data: ', error));
   }, []);
 
-  const handleImageClick = (image) => {
-    setSelectedImage(image);
-    setShowImage(true);
+  // ランダムに画像を選択して表示する関数
+  const showRandomImage = () => {
+    selectRandomImage();
+    setShowImage(true); // 画像を表示する
   };
 
   // ランダムに画像を選択する関数
   const selectRandomImage = () => {
     if (images.length > 0) {
       const randomIndex = Math.floor(Math.random() * images.length);
-      const randomImage = images[randomIndex].direct_link;
-      setSelectedImage(randomImage);
+      setSelectedImage(images[randomIndex].direct_link);
     }
   };
 
   return (
     <div>
-      {images.map(image => (
-        <div key={image.name} onClick={() => handleImageClick(image.direct_link)}>
-          <img src={image.direct_link} alt={image.name} style={{ width: '100px', height: 'auto' }} />
-          <p>{image.name}</p>
-        </div>
-      ))}
-      {showImage && (
+      <button onClick={showRandomImage}>Show Random Image</button>
+      {showImage && selectedImage && ( // showImageとselectedImageが真の時のみ表示
         <div>
           <img src={selectedImage} alt="Selected" style={{ width: '300px', height: 'auto' }} />
         </div>
       )}
-      <button onClick={selectRandomImage}>Show Random Image</button>
     </div>
   );
 }
