8a6abfe2715562eaf8dd05b40cff924c04d92c6a
伊藤 一希 <kzkio114@gmail.com>
2024-03-10 19:23:42 +0900

    空白修正

--- backend/app/controllers/api/v1/images_controller.rb
+++ backend/app/controllers/api/v1/images_controller.rb
@@ -8,6 +8,7 @@ module Api
           initialize_google_api # GoogleApiConcernモジュールで定義されたメソッドを呼び出し
           render json: @images # 画像データをJSON形式でフロントエンドに送信
         end
+      
       end
     end
   end
