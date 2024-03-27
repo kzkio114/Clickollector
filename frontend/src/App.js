ffb7df223f3b4ce30c3a2818ffe197244e02d5f4
伊藤 一希 <kzkio114@gmail.com>
2024-03-27 14:28:06 +0900

    database修正

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <h1>ゲーム作成中</h1>
+        <h1>作成中</h1>
         <Routes>
           <Route path="/" element={
             <>
